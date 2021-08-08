8494e8d608e8e8f12f2bb53ab0e169b98b0b2926
maroon <maroonay@gmail.com>
2021-08-08 15:14:00 +0300

    added support for hybrid compliance

--- bundle/status/base_compliance_status_bundle.go
+++ bundle/status/base_compliance_status_bundle.go
@@ -1,17 +1,34 @@
 package status
 
-// PolicyComplianceStatus struct holds information for policy compliance status.
-type PolicyComplianceStatus struct {
+// PolicyCompleteComplianceStatus struct holds information for (optimized) policy compliance status.
+type PolicyCompleteComplianceStatus struct {
 	PolicyID                  string   `json:"policyId"`
 	NonCompliantClusters      []string `json:"nonCompliantClusters"`
 	UnknownComplianceClusters []string `json:"unknownComplianceClusters"`
 	ResourceVersion           string   `json:"resourceVersion"`
 }
 
-// BaseComplianceStatusBundle is the base struct for compliance status bundle.
-type BaseComplianceStatusBundle struct {
-	Objects              []*PolicyComplianceStatus `json:"objects"`
-	LeafHubName          string                    `json:"leafHubName"`
-	BaseBundleGeneration uint64                    `json:"baseBundleGeneration"`
-	Generation           uint64                    `json:"generation"`
+// PolicyDeltaComplianceStatus struct holds information for raw policy compliance status.
+type PolicyDeltaComplianceStatus struct {
+	PolicyID                  string   `json:"policyId"`
+	CompliantClusters         []string `json:"compliantClusters"`
+	NonCompliantClusters      []string `json:"nonCompliantClusters"`
+	UnknownComplianceClusters []string `json:"unknownComplianceClusters"`
+	ResourceVersion           string   `json:"resourceVersion"`
+}
+
+// BaseCompleteComplianceStatusBundle is the base struct for complete-compliance status bundle.
+type BaseCompleteComplianceStatusBundle struct {
+	Objects              []*PolicyCompleteComplianceStatus `json:"objects"`
+	LeafHubName          string                            `json:"leafHubName"`
+	BaseBundleGeneration uint64                            `json:"baseBundleGeneration"`
+	Generation           uint64                            `json:"generation"`
+}
+
+// BaseDeltaComplianceStatusBundle is the base struct for delta-compliance status bundle.
+type BaseDeltaComplianceStatusBundle struct {
+	Objects              []*PolicyDeltaComplianceStatus `json:"objects"`
+	LeafHubName          string                         `json:"leafHubName"`
+	BaseBundleGeneration uint64                         `json:"baseBundleGeneration"`
+	Generation           uint64                         `json:"generation"`
 }
