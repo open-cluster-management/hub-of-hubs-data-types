e1f39737b5cd5f4055923ca44c32f7ead13c3df5
golanshabi <golanshabi3@gmail.com>
2021-08-26 12:45:59 +0300

    Added new Root reference

--- fields.go
+++ fields.go
@@ -6,4 +6,7 @@ const (
 
 	// OriginOwnerReferenceAnnotation - Origin owner reference annotation.
 	OriginOwnerReferenceAnnotation = "hub-of-hubs.open-cluster-management.io/originOwnerReferenceUid"
+
+	// RootReferenceLabel - The reference for when a policy propagates from a managed cluster.
+	RootReferenceLabel = "policy.open-cluster-management.io/root-policy"
 )
