bf3970a8f22c0601bf25c247b30610ef7e77f1e5
golanshabi <golanshabi3@gmail.com>
2021-08-26 14:11:16 +0300

    Removed unused fields

--- fields.go
+++ fields.go
@@ -6,7 +6,4 @@ const (
 
 	// OriginOwnerReferenceAnnotation - Origin owner reference annotation.
 	OriginOwnerReferenceAnnotation = "hub-of-hubs.open-cluster-management.io/originOwnerReferenceUid"
-
-	// RootReferenceLabel - The reference for when a policy propagates from a managed cluster.
-	RootReferenceLabel = "policy.open-cluster-management.io/root-policy"
 )
