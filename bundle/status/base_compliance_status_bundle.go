fa5e05856eb68ba3a7dfcd4312ef8bd3a90c3b86
maroon <maroonay@gmail.com>
2021-09-22 10:17:07 +0300

    reverted delta to having 1 base + turned generation field to string in all bundle types.

--- bundle/status/base_compliance_status_bundle.go
+++ bundle/status/base_compliance_status_bundle.go
@@ -32,21 +32,3 @@ type BaseDeltaComplianceStatusBundle struct {
 	BaseBundleGeneration uint64                         `json:"baseBundleGeneration"`
 	Generation           string                         `json:"generation"`
 }
-
-// TODO to be deleted after merging kafka changes, for backward compatibility until other repos adapt new types
-
-// PolicyComplianceStatus struct holds information for policy compliance status.
-type PolicyComplianceStatus struct {
-	PolicyID                  string   `json:"policyId"`
-	NonCompliantClusters      []string `json:"nonCompliantClusters"`
-	UnknownComplianceClusters []string `json:"unknownComplianceClusters"`
-	ResourceVersion           string   `json:"resourceVersion"`
-}
-
-// BaseComplianceStatusBundle is the base struct for compliance status bundle.
-type BaseComplianceStatusBundle struct {
-	Objects              []*PolicyComplianceStatus `json:"objects"`
-	LeafHubName          string                    `json:"leafHubName"`
-	BaseBundleGeneration uint64                    `json:"baseBundleGeneration"`
-	Generation           uint64                    `json:"generation"`
-}
