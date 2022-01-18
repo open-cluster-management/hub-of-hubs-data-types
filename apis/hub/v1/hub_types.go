cd638f2b7bf6b0fa8a6ac7f1d11e4301ef67c0cd
maroon <maroon.ayoub@ibm.com>
2022-01-18 11:20:27 +0200

    hub status

--- apis/hub/v1/hub_types.go
+++ apis/hub/v1/hub_types.go
@@ -13,7 +13,13 @@ type HubSpec struct {
 }
 
 // HubStatus defines the observed state of Hub.
-type HubStatus struct{}
+type HubStatus struct {
+	ActiveUUID                string `json:"active-uuid,omitempty"`
+	TransportBootstrapServers string `json:"transport-bootstrap-servers,omitempty"`
+	TransportCertificate      string `json:"transport-certificate,omitempty"`
+	TransportSpecTopic        string `json:"transport-spec-topic,omitempty"`
+	WriterIncarnation         string `json:"writer-incarnation,omitempty"`
+}
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
