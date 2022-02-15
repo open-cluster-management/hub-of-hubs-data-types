0e268091c77fde3ff3ab5d23904712c1244c0897
maroon <maroon.ayoub@ibm.com>
2022-02-15 14:34:25 +0200

    changed entries version to timestamp

--- bundle/spec/managed_cluster_metadata.go
+++ bundle/spec/managed_cluster_metadata.go
@@ -1,10 +1,12 @@
 package spec
 
+import "time"
+
 // ManagedClusterMetadata struct holds information for managed cluster metadata.
 type ManagedClusterMetadata struct {
 	Name             string            `json:"name"`
 	Namespace        string            `json:"namespace"`
 	Labels           map[string]string `json:"labels"`
 	DeletedLabelKeys []string          `json:"deletedLabelKeys"`
-	Version          string            `json:"version"`
+	UpdateTimestamp  time.Time         `json:"updateTimestamp"`
 }
