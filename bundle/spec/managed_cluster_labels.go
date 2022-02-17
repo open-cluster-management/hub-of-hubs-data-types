4966565e22c937ba493730ad697cc9af0328bb85
maroon <maroon.ayoub@ibm.com>
2022-02-17 16:38:44 +0200

    removed namespace from MC labels spec struct and added version

--- bundle/spec/managed_cluster_labels.go
+++ bundle/spec/managed_cluster_labels.go
@@ -1,12 +1,14 @@
 package spec
 
-import "time"
+import (
+	"time"
+)
 
 // ManagedClusterLabelsSpec struct holds information for managed cluster metadata.
 type ManagedClusterLabelsSpec struct {
 	Name             string            `json:"name"`
-	Namespace        string            `json:"namespace"`
 	Labels           map[string]string `json:"labels"`
 	DeletedLabelKeys []string          `json:"deletedLabelKeys"`
 	UpdateTimestamp  time.Time         `json:"updateTimestamp"`
+	Version          int64             `json:"version"`
 }
