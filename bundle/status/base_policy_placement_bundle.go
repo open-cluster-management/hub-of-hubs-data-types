b2ec8ee44d23779a79153819b405f8852d6e19b6
Nir Rozenbaum <nirro@il.ibm.com>
2022-03-27 12:31:03 +0300

    added support for multiple placements in a single policy
    Signed-off-by: Nir Rozenbaum <nirro@il.ibm.com>

--- bundle/status/base_policy_placement_bundle.go
+++ bundle/status/base_policy_placement_bundle.go
@@ -1,10 +1,11 @@
 package status
 
+import policiesv1 "github.com/open-cluster-management/governance-policy-propagator/api/v1"
+
 // PolicyPlacementStatus holds information for policy placement status.
 type PolicyPlacementStatus struct {
-	PolicyID         string `json:"policyId"`
-	PlacementRule    string `json:"placementRule"`
-	PlacementBinding string `json:"placementBinding"`
+	PolicyID  string                  `json:"policyId"`
+	Placement []*policiesv1.Placement `json:"placement"`
 }
 
 // BasePolicyPlacementStatusBundle the base struct for policy placement status bundle.
